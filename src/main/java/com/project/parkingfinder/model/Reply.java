f56c2bf39a3cf8417526cb1a9adcb5519b835bb8
lancer2672 <JBkhanhtran@gmail.com>
2024-11-25 19:42:48 +0700

    Feat: reviewe &reply models

---
+++ src/main/java/com/project/parkingfinder/model/Reply.java
@@ -0,0 +1,41 @@
+package com.project.parkingfinder.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "replies")
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String comment;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+}
